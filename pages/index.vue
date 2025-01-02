e7eca0a4719a74a56455968a435acb60d87edaf0
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-01-02 06:49:55 +0100

    :rocket: Setup default layout and home page

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+  <div>
+    <h1>{{ t("helloWorld") }}</h1>
+    <Button>{{ t("helloWorld") }}</Button>
+  </div>
+</template>
