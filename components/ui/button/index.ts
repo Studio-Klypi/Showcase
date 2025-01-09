b9fe5e68fab9782520f12321f33063e3e7dbf3c0
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-01-09 13:25:23 +0100

    :lipstick: Add styles to shadcn button

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -13,14 +13,17 @@ export const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        primaryOutline: "border border-primary-foreground bg-primary shadow-sm hover:bg-black/5",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        discretLink: "underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
+        discret: "h-auto rounded-md px-0",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
       },
