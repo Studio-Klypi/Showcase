a5e5161728fde073a4ce6ee71a2718c26eec4c06
loicmaes <production@maesloic.fr>
2025-01-09 17:12:43 +0100

    :rocket: Add missing pages

---
+++ pages/legal/cgv.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PageRoot from "~/components/composing/PageRoot.vue";
+</script>
+
+<template>
+  <PageRoot name="legal/cgv">
+    COMING SOON...
+  </PageRoot>
+</template>
---
+++ pages/team.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PageRoot from "~/components/composing/PageRoot.vue";
+</script>
+
+<template>
+  <PageRoot name="team">
+    COMING SOON...
+  </PageRoot>
+</template>
